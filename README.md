f008b3ffcaaa7108a289d34505e0c5f09437be54
Mo <moho2010@live.ie>
2025-02-23 13:30:25 -0500

    test commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + Vite
+
+A project recreating the apple website for the iphone launch. using it to practise working with React and 3d models on page
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), sentryVitePlugin({
+    org: "jsm-x3l",
+    project: "javascript-react"
+  })],
+
+  build: {
+    target: "esnext",
+    sourcemap: true
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "esnext",
+    },
+  },
+});
